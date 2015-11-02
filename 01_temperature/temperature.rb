353ff8b7fd2d0b30e66cdb374236f19f05131019
SCOTT WALSH <SCMWALSH@GMAIL.COM>
2015-11-02 14:40:11 -0500

    temperature test complete

---
+++ 01_temperature/temperature.rb
@@ -0,0 +1,18 @@
+def ftoc(temp)
+
+	( temp - 32.0 ) * ( 5.0 / 9.0 )
+	# if temp == 32
+	# 	0
+	# elsif temp == 98.6
+	# 	37
+	# elsif  temp == 68
+	# 		20
+	# else
+	# 	100
+	# end
+end
+
+def ctof(temp)
+	( temp * ( 9.0 / 5.0 ) + 32 )
+
+end
